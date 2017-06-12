a13ba3f8a0e3b43eb576f16a4c71b2f2e2107ec0
karyvdj <kaina51@hotmail.com>
2017-06-12 13:08:33 -0500

    Agregando plantilla de modal dinamico y creando funciones especificas

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,70 @@
-$.getJSON("http://pokeapi.co/api/v2/pokemon/",
-	function(response){
+var cargarPagina = function () {
+	cargarPokemons();
+	$(document).on("click", ".boton-info", detallePokemon);
+
+}
+
+var cargarPokemons = function () {
+	var url = "http://pokeapi.co/api/v2/pokemon/";
+	$.getJSON(url,function (response){
 		var pokemons = response.results;
 		crearPokemons(pokemons);
 	});
+}
+
 
 function crearPokemons(pokemons) {
 	var contenedorPokemons = $("#pokemons");
 	var fila = $("<article/>", {"class": "row center"});
 	$(pokemons).each(function (i,pokemon) {
-		var contenedorImg = $("<div/>", {"class": "col m3 s6 carta hoverable"}).attr("data-info",pokemon.url);
-		var imgPokemon = $("<img/>").attr("src",imagenesPokemons[i].imagen);
-		var nombrePokemon = $("<a/>", {"class":" waves-light btn margin"}).text(pokemon.name).attr('href',"#modalPokemon");
+		var contenedorImg = $("<div/>", {"class": "col m3 s6 carta hoverable"});
+		contenedorImg.attr("data-info",pokemon.url);
+		var imgPokemon = $("<img/>");
+		imgPokemon.attr("src",imagenesPokemons[i].imagen);
+		var nombrePokemon = $("<a/>", {"class":" waves-light btn margin boton-info"});
+		nombrePokemon.text(pokemon.name);
+		nombrePokemon.attr('href',"#modalPokemon");
 
 		contenedorImg.append(imgPokemon).append(nombrePokemon);
 		fila.append(contenedorImg);
 		contenedorPokemons.append(fila);
 	});
 }
+
+var pantillaModal =
+	"<section id='modalPokemon' class='modal'>"+
+			"<article class='modal-content'>"+
+					"<div class='row'>"+
+							"<div class='col s6'>"
+									"<img id='imgPokemon' src='imagenesPokemons[i].imagen' alt='nombre'>"+
+							"</div>"+
+							"<div class='col s6'>"+
+								"<h4>__nombre__</h4>"+
+								"<p>"+
+									"Tipo:"+
+									"<strong>agua</strong>"+
+								"</p>"+
+								"<p><strong>Poder: </strong>__nombre__</p>"+
+								"<p><strong>Vida: </strong>__habilidad__</p>"
+							"</div>"+
+					"</div>"+
+			"</article>"+
+			"<div class='modal-footer>"+
+				"<a href='#' class='modal-action modal-close waves-effect waves-green btn-floating grey center righ'>&#215;</a>"+
+			"</div>"+
+	"</section>";
+
+var detallePokemon = function () {
+  var url = $(this).data("url");
+    var modal = $("#modalPokemon");
+    $.getJSON(url , function (response) {
+      modal.html(
+        pantillaModal.replace("__nombre__", response.name)
+        .replace("__habilidad__", response.ability)
+      );
+    });
+}
+
 var imagenesPokemons = [
     {
         "imagen" : "assets/img/1-bulbasaur.png"
@@ -79,6 +127,8 @@ var imagenesPokemons = [
         "imagen" : "assets/img/20-raticate.png"
     }
 ];
+
+$(document).ready(cargarPagina);
 
 //-------------JS----------------
 // var xhr = new XMLHttpRequest();
